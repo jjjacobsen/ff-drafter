f8370dff42fbbf2136e20faed23dbc810177d1b3
Jonah Jacobsen <jjacobsen115@gmail.com>
2025-09-01 11:30:32 -0500

    add dotenv and properly setup callinlg my league with the api

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import os
+
+from dotenv import load_dotenv
+from espn_api.football import League
+
+
 def main():
+    league = League(
+        league_id=os.getenv("LEAGUE_ID"),
+        year=2024,
+        espn_s2=os.getenv("ESPN_S2"),
+        swid=os.getenv("SWID"),
+    )
     print("Hello from ff-drafter!")
+    print(league.teams)
 
 
 if __name__ == "__main__":
+    load_dotenv()
     main()
